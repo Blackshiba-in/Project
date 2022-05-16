04734faec8a86ec32356a893327c60492fd85d19
Blackshiba-in <86581481+Blackshiba-in@users.noreply.github.com>
2022-05-16 13:27:59 +0800

    Create Owner.sol

---
+++ Project Red Roc/Owner.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Unlicensed
+ 
+pragma solidity ^0.8.4;
+
+import "./Address.sol";
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+  
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+    //Locks the contract for owner for the amount of time provided
+    function renouncedOwner() public virtual onlyOwner {
+
+         emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+  
+}
